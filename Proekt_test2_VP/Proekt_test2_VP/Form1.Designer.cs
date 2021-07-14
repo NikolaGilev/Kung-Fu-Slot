67b1f7aea09437ca3196023b746e5f10fa15033c
MarkoZoreski <83420035+MarkoZoreski@users.noreply.github.com>
2021-07-14 17:11:08 +0200

    Add files via upload
    added music to main slot

--- Proekt_test2_VP/Proekt_test2_VP/Form1.Designer.cs
+++ Proekt_test2_VP/Proekt_test2_VP/Form1.Designer.cs
@@ -30,12 +30,13 @@ namespace Proekt_test2_VP
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.creditLabel = new System.Windows.Forms.Label();
+            this.BetLabel = new System.Windows.Forms.Label();
+            this.WinLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
@@ -47,6 +48,8 @@ namespace Proekt_test2_VP
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.BetAmountNumeric = new System.Windows.Forms.NumericUpDown();
+            this.musicImage = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -59,67 +62,79 @@ namespace Proekt_test2_VP
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BetAmountNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicImage)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(29, 47);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 58);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox1.Size = new System.Drawing.Size(192, 149);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(207, 47);
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Location = new System.Drawing.Point(517, 58);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox3.Size = new System.Drawing.Size(192, 149);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(384, 47);
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(281, 58);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox2.Size = new System.Drawing.Size(192, 149);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
-            // label1
+            // creditLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Credits: 100";
+            this.creditLabel.AutoSize = true;
+            this.creditLabel.Location = new System.Drawing.Point(35, 27);
+            this.creditLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.creditLabel.Name = "creditLabel";
+            this.creditLabel.Size = new System.Drawing.Size(84, 17);
+            this.creditLabel.TabIndex = 4;
+            this.creditLabel.Text = "Credits: 100";
             // 
-            // label2
+            // BetLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(204, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Bet: 5";
+            this.BetLabel.AutoSize = true;
+            this.BetLabel.Location = new System.Drawing.Point(36, 615);
+            this.BetLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BetLabel.Name = "BetLabel";
+            this.BetLabel.Size = new System.Drawing.Size(37, 17);
+            this.BetLabel.TabIndex = 5;
+            this.BetLabel.Text = "Bet: ";
             // 
-            // label3
+            // WinLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(381, 22);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Win: 0";
+            this.WinLabel.AutoSize = true;
+            this.WinLabel.Location = new System.Drawing.Point(508, 27);
+            this.WinLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.WinLabel.Name = "WinLabel";
+            this.WinLabel.Size = new System.Drawing.Size(48, 17);
+            this.WinLabel.TabIndex = 6;
+            this.WinLabel.Text = "Win: 0";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 512);
+            this.button1.Location = new System.Drawing.Point(436, 613);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 7;
             this.button1.Text = "Spin";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,90 +146,141 @@ namespace Proekt_test2_VP
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(388, 191);
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Location = new System.Drawing.Point(753, 58);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox4.Size = new System.Drawing.Size(192, 149);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 10;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(211, 191);
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Location = new System.Drawing.Point(39, 235);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox5.Size = new System.Drawing.Size(192, 149);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(33, 191);
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Location = new System.Drawing.Point(281, 235);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox6.Size = new System.Drawing.Size(192, 149);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(384, 336);
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Location = new System.Drawing.Point(517, 235);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox7.Size = new System.Drawing.Size(192, 149);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 13;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
-            this.pictureBox8.Location = new System.Drawing.Point(207, 336);
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.Location = new System.Drawing.Point(753, 235);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox8.Size = new System.Drawing.Size(192, 149);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox8.TabIndex = 12;
             this.pictureBox8.TabStop = false;
             // 
             // pictureBox9
             // 
-            this.pictureBox9.Location = new System.Drawing.Point(29, 336);
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.Location = new System.Drawing.Point(39, 414);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox9.Size = new System.Drawing.Size(192, 149);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox9.TabIndex = 11;
             this.pictureBox9.TabStop = false;
             // 
             // pictureBox10
             // 
-            this.pictureBox10.Location = new System.Drawing.Point(561, 336);
+            this.pictureBox10.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox10.Location = new System.Drawing.Point(281, 414);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox10.Size = new System.Drawing.Size(192, 149);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox10.TabIndex = 16;
             this.pictureBox10.TabStop = false;
             // 
             // pictureBox11
             // 
-            this.pictureBox11.Location = new System.Drawing.Point(565, 191);
+            this.pictureBox11.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox11.Location = new System.Drawing.Point(517, 425);
+            this.pictureBox11.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox11.Size = new System.Drawing.Size(192, 149);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox11.TabIndex = 15;
             this.pictureBox11.TabStop = false;
             // 
             // pictureBox12
             // 
-            this.pictureBox12.Location = new System.Drawing.Point(561, 47);
+            this.pictureBox12.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox12.Location = new System.Drawing.Point(753, 414);
+            this.pictureBox12.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox12.Size = new System.Drawing.Size(192, 149);
             this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox12.TabIndex = 14;
             this.pictureBox12.TabStop = false;
             // 
+            // BetAmountNumeric
+            // 
+            this.BetAmountNumeric.Location = new System.Drawing.Point(69, 613);
+            this.BetAmountNumeric.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.BetAmountNumeric.Name = "BetAmountNumeric";
+            this.BetAmountNumeric.Size = new System.Drawing.Size(91, 22);
+            this.BetAmountNumeric.TabIndex = 17;
+            this.BetAmountNumeric.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.BetAmountNumeric.ValueChanged += new System.EventHandler(this.BetAmountNumeric_ValueChanged);
+            // 
+            // musicImage
+            // 
+            this.musicImage.BackColor = System.Drawing.Color.Transparent;
+            this.musicImage.Location = new System.Drawing.Point(845, 613);
+            this.musicImage.Name = "musicImage";
+            this.musicImage.Size = new System.Drawing.Size(56, 40);
+            this.musicImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.musicImage.TabIndex = 18;
+            this.musicImage.TabStop = false;
+            this.musicImage.Click += new System.EventHandler(this.musicImage_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 555);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(996, 683);
+            this.Controls.Add(this.musicImage);
+            this.Controls.Add(this.BetAmountNumeric);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.pictureBox12);
@@ -225,12 +291,13 @@ namespace Proekt_test2_VP
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WinLabel);
+            this.Controls.Add(this.BetLabel);
+            this.Controls.Add(this.creditLabel);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -246,6 +313,8 @@ namespace Proekt_test2_VP
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BetAmountNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,9 +325,9 @@ namespace Proekt_test2_VP
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label creditLabel;
+        private System.Windows.Forms.Label BetLabel;
+        private System.Windows.Forms.Label WinLabel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox4;
@@ -270,6 +339,8 @@ namespace Proekt_test2_VP
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.PictureBox pictureBox11;
         private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.NumericUpDown BetAmountNumeric;
+        private System.Windows.Forms.PictureBox musicImage;
     }
 }
 
--- Proekt_test2_VP/Proekt_test2_VP/Form1.cs
+++ Proekt_test2_VP/Proekt_test2_VP/Form1.cs
@@ -7,16 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace Proekt_test2_VP
 {
     public partial class Form1 : Form
     {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
         public Form1()
         {
             InitializeComponent();
             Randomizer Randomizer = new Randomizer();
             DoubleBuffered = true;
+            musicImage.Image = Image.FromFile("mute.png");
+            musicImage.Tag = "mute.png";
+
         }
         // rand p-s
         public static int p1;
@@ -42,15 +47,14 @@ namespace Proekt_test2_VP
             pictureBox1.Image = Image.FromFile("1.png");
             pictureBox2.Image = Image.FromFile("2.png");
             pictureBox3.Image = Image.FromFile("3.png");
-
             pictureBox4.Image = Image.FromFile("1.png");
+            
             pictureBox5.Image = Image.FromFile("2.png");
             pictureBox6.Image = Image.FromFile("3.png");
-
             pictureBox7.Image = Image.FromFile("1.png");
             pictureBox8.Image = Image.FromFile("2.png");
-            pictureBox9.Image = Image.FromFile("3.png");
 
+            pictureBox9.Image = Image.FromFile("3.png");
             pictureBox10.Image = Image.FromFile("1.png");
             pictureBox11.Image = Image.FromFile("2.png");
             pictureBox12.Image = Image.FromFile("3.png");
@@ -58,11 +62,8 @@ namespace Proekt_test2_VP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (credits >= bet)
+            if ( credits >= bet || timer1.Enabled )
             {
-                credits -= bet;
-                label1.Text = "Credits: " + credits.ToString();
-
                 Rolling();
                 if (timer1.Enabled)
                 {
@@ -71,6 +72,8 @@ namespace Proekt_test2_VP
                 }
                 else
                 {
+                    credits -= bet;
+                    creditLabel.Text = "Credits: " + credits.ToString();
                     timer1.Start();
                 }
 
@@ -83,7 +86,7 @@ namespace Proekt_test2_VP
         private void Pay()
         {
             // GET RESULTS FROM PAYTABLE
-            // CHECK IF 1, 2 OR 3 OCCURANCES
+            // CHECK IF 1, 2, 3 or 4 OCCURANCES
             if (p1 == 3) total = total + 5;
 
             if (p1 == 2 & p2 == 2) total = total + 10;
@@ -96,52 +99,48 @@ namespace Proekt_test2_VP
             credits = credits + total;
 
 
-            label3.Text = "Win: " + total.ToString();
-            label1.Text = "Credits: " + credits.ToString();
-            label2.Text = "Bet: " + bet.ToString();
+            WinLabel.Text = "Win: " + total.ToString();
+            creditLabel.Text = "Credits: " + credits.ToString();
+            BetLabel.Text = "Bet: " + bet.ToString();
         }
 
         private void Rolling()
         {
-            for (var i = 0; i < 10; i++)
-            {
-                p1 = Randomizer.getRand(1, 4);
-                p2 = Randomizer.getRand(1, 4);
-                p3 = Randomizer.getRand(1, 4);
-                
-                p4 = Randomizer.getRand(1, 4);
-                p5 = Randomizer.getRand(1, 4);
-                p6 = Randomizer.getRand(1, 4);
-                
-                p7 = Randomizer.getRand(1, 4);
-                p8 = Randomizer.getRand(1, 4);
-                p9 = Randomizer.getRand(1, 4);
+            p1 = Randomizer.getRand(1, 4);
+            p2 = Randomizer.getRand(1, 4);
+            p3 = Randomizer.getRand(1, 4);
+            p4 = Randomizer.getRand(1, 4);
 
-                p10 = Randomizer.getRand(1, 4);
-                p11 = Randomizer.getRand(1, 4);
-                p12 = Randomizer.getRand(1, 4);
-            }
+            p5 = Randomizer.getRand(1, 4);
+            p6 = Randomizer.getRand(1, 4);
+            p7 = Randomizer.getRand(1, 4);
+            p8 = Randomizer.getRand(1, 4);
+
+            p9 = Randomizer.getRand(1, 4);
+            p10 = Randomizer.getRand(1, 4);
+            p11 = Randomizer.getRand(1, 4);
+            p12 = Randomizer.getRand(1, 4);
+
             if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
             pictureBox1.Image = Image.FromFile(p1.ToString() + ".png");
             if (pictureBox2.Image != null) pictureBox2.Image.Dispose();
             pictureBox2.Image = Image.FromFile(p2.ToString() + ".png");
             if (pictureBox3.Image != null) pictureBox3.Image.Dispose();
             pictureBox3.Image = Image.FromFile(p3.ToString() + ".png");
-
             if (pictureBox4.Image != null) pictureBox4.Image.Dispose();
             pictureBox4.Image = Image.FromFile(p4.ToString() + ".png");
+
             if (pictureBox5.Image != null) pictureBox5.Image.Dispose();
             pictureBox5.Image = Image.FromFile(p5.ToString() + ".png");
             if (pictureBox6.Image != null) pictureBox6.Image.Dispose();
             pictureBox6.Image = Image.FromFile(p6.ToString() + ".png");
-
             if (pictureBox7.Image != null) pictureBox7.Image.Dispose();
             pictureBox7.Image = Image.FromFile(p7.ToString() + ".png");
             if (pictureBox8.Image != null) pictureBox8.Image.Dispose();
             pictureBox8.Image = Image.FromFile(p8.ToString() + ".png");
+
             if (pictureBox9.Image != null) pictureBox9.Image.Dispose();
             pictureBox9.Image = Image.FromFile(p9.ToString() + ".png");
-
             if (pictureBox10.Image != null) pictureBox10.Image.Dispose();
             pictureBox10.Image = Image.FromFile(p10.ToString() + ".png");
             if (pictureBox11.Image != null) pictureBox11.Image.Dispose();
@@ -153,6 +152,42 @@ namespace Proekt_test2_VP
         private void timer1_Tick(object sender, EventArgs e)
         {
             Rolling();
+            if (musicImage.Tag.Equals("unmute.png") && player.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                player.URL = "OogwayAscends.mp3";
+                player.controls.play();
+            }
+        }
+
+        private void BetAmountNumeric_ValueChanged(object sender, EventArgs e)
+        {
+            if(BetAmountNumeric.Value > credits)
+            {
+                BetAmountNumeric.Value = credits;
+            }
+            if(BetAmountNumeric.Value >= 5)
+            {
+                bet = Convert.ToInt64(BetAmountNumeric.Value);
+            }
+            
+        }
+
+
+        private void musicImage_Click(object sender, EventArgs e)
+        {
+            if (musicImage.Tag.Equals("mute.png"))
+            {
+                musicImage.Image = Image.FromFile("unmute.png");
+                musicImage.Tag = "unmute.png";
+                player.URL = "OogwayAscends.mp3";
+                player.controls.play();
+            }
+            else
+            {
+                musicImage.Image = Image.FromFile("mute.png");
+                musicImage.Tag = "mute.png";
+                player.controls.stop();
+            }
         }
     }
 }
