12629188d715e518d268fd837dfa70829c7a29da
NikolaGilev <nikola.gilev@students.finki.ukim.mk>
2021-07-20 21:30:47 +0200

    Revert "mergeTest"
    This reverts commit 13978d146f417cc557844a92b6c26105b2c65c7e.

--- Proekt_test2_VP/Proekt_test2_VP/Form1.cs
+++ Proekt_test2_VP/Proekt_test2_VP/Form1.cs
@@ -13,9 +13,7 @@ namespace Proekt_test2_VP
 {
     public partial class Form1 : Form
     {
-        WindowsMediaPlayer musicPlayer = new WindowsMediaPlayer();
-        public PlayerClass player = new PlayerClass();
-
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
         public Form1()
         {
             InitializeComponent();
@@ -23,34 +21,52 @@ namespace Proekt_test2_VP
             DoubleBuffered = true;
             musicImage.Image = Image.FromFile("mute.png");
             musicImage.Tag = "mute.png";
-            creditLabel.Text = "Credits: " + player.Credits.ToString();
+
         }
+        // rand p-s
+        //public static int p1;
+        //public static int p2;
+        //public static int p3;
+        //public static int p4;
+        //public static int p5;
+        //public static int p6;
+        //public static int p7;
+        //public static int p8;
+        //public static int p9;
+        //public static int p10;
+        //public static int p11;
+        //public static int p12;
 
         public static int[] p = new int[12];
 
+        // CREDIT, TOTAL AND BET
+        public static double credits = 100;
+        public static double total = 0;
+        public static long bet = 5;
+
         private void Form1_Load(object sender, EventArgs e)
-            {
-            pictureBox1.Image = Image.FromFile("Pictures/1.png");
-            pictureBox2.Image = Image.FromFile("Pictures/2.png");
-            pictureBox3.Image = Image.FromFile("Pictures/3.png");
-            pictureBox4.Image = Image.FromFile("Pictures/1.png");
+        {
+            pictureBox1.Image = Image.FromFile("1.png");
+            pictureBox2.Image = Image.FromFile("2.png");
+            pictureBox3.Image = Image.FromFile("3.png");
+            pictureBox4.Image = Image.FromFile("1.png");
 
-            pictureBox5.Image = Image.FromFile("Pictures/2.png");
-            pictureBox6.Image = Image.FromFile("Pictures/3.png");
-            pictureBox7.Image = Image.FromFile("Pictures/1.png");
-            pictureBox8.Image = Image.FromFile("Pictures/2.png");
+            pictureBox5.Image = Image.FromFile("2.png");
+            pictureBox6.Image = Image.FromFile("3.png");
+            pictureBox7.Image = Image.FromFile("1.png");
+            pictureBox8.Image = Image.FromFile("2.png");
 
-            pictureBox9.Image = Image.FromFile("Pictures/3.png");
-            pictureBox10.Image = Image.FromFile("Pictures/1.png");
-            pictureBox11.Image = Image.FromFile("Pictures/2.png");
-            pictureBox12.Image = Image.FromFile("Pictures/3.png");
-            creditLabel.Text = "Credits: " + player.Credits.ToString();
+            pictureBox9.Image = Image.FromFile("3.png");
+            pictureBox10.Image = Image.FromFile("1.png");
+            pictureBox11.Image = Image.FromFile("2.png");
+            pictureBox12.Image = Image.FromFile("3.png");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (player.Credits >= player.Bet || timer1.Enabled)
+            if (credits >= bet || timer1.Enabled)
             {
+                Rolling();
                 if (timer1.Enabled)
                 {
                     timer1.Stop();
@@ -58,15 +74,97 @@ namespace Proekt_test2_VP
                 }
                 else
                 {
-                    player.Spin();
-                    creditLabel.Text = "Credits: " + player.Credits.ToString();
+                    credits -= bet;
+                    creditLabel.Text = "Credits: " + credits.ToString();
                     timer1.Start();
                 }
-                player.Total = 0;
+
+                total = 0;
 
             }
 
         }
+
+        private void Connected3(int px)
+        {
+            if (px == 1) total = total + bet * 2;
+
+            if (px == 2) total = total + bet * 1.5;
+
+            if (px == 3) total = total + bet * 1;
+
+            if (px == 4) total = total + bet * 0.5;
+
+            //if (p1 == 5) total = total + bet;
+
+            //if (p1 == 6) total = total + bet;
+
+            //if (p1 == 7) total = total + bet;
+
+            //if (p1 == 8) total = total + bet;
+
+            //if (p1 == 9) total = total + bet;
+
+            //if (p1 == 10) total = total + bet;
+
+            //if (p1 == 11) total = total + bet;
+
+            //if (p1 == 12) total = total + bet;
+        }
+        private void Connected4(int px)
+        {
+            if (px == 1) total = total + bet * 5;
+
+            if (px == 2) total = total + bet * 3;
+
+            if (px == 3) total = total + bet * 2;
+
+            if (px == 4) total = total + bet * 1;
+
+            //if (p1 == 5) total = total + bet;
+
+            //if (p1 == 6) total = total + bet;
+
+            //if (p1 == 7) total = total + bet;
+
+            //if (p1 == 8) total = total + bet;
+
+            //if (p1 == 9) total = total + bet;
+
+            //if (p1 == 10) total = total + bet;
+
+            //if (p1 == 11) total = total + bet;
+
+            //if (p1 == 12) total = total + bet;
+        }
+        private void ZigZag(int px)
+        {
+            if (px == 1) total = total + bet * 6;
+
+            if (px == 2) total = total + bet * 5;
+
+            if (px == 3) total = total + bet * 4;
+
+            if (px == 4) total = total + bet * 3;
+
+            //if (p1 == 5) total = total + bet;
+
+            //if (p1 == 6) total = total + bet;
+
+            //if (p1 == 7) total = total + bet;
+
+            //if (p1 == 8) total = total + bet;
+
+            //if (p1 == 9) total = total + bet;
+
+            //if (p1 == 10) total = total + bet;
+
+            //if (p1 == 11) total = total + bet;
+
+            //if (p1 == 12) total = total + bet;
+        }
+
+
         private void Pay()
         {
             // GET RESULTS FROM PAYTABLE
@@ -94,7 +192,7 @@ namespace Proekt_test2_VP
 
                     if (couter == 4)
                     {
-                        player.getConnected4(px);
+                        Connected4(px);
                         connect3 = false;
                     }
 
@@ -103,7 +201,7 @@ namespace Proekt_test2_VP
 
                 if (connect3) // povika funkcija za da isplati 3 spoeni
                 {
-                    player.getConnected3(px);
+                    Connected3(px);
 
                 }
 
@@ -113,99 +211,101 @@ namespace Proekt_test2_VP
             }
 
             // for zig-zag 4 connections :
-            if (p[0] == p[5] & p[10] == p[7] & p[5] == p[10] & p[0] != 8)
-            {
-                player.getZigZag(px);
-            }
-            if (p[1] == p[4] & p[6] == p[11] & p[4] == p[6] & p[1] != 8)
-            {
-                player.getZigZag(px);
-            }
-            if (p[2] == p[5] & p[8] == p[7] & p[5] == p[8] & p[2] != 8)
-            { 
-                player.getZigZag(px);
-            }
-            if (p[3] == p[6] & p[9] == p[4] & p[6] == p[9] & p[3] != 8)
-            {
-                player.getZigZag(px);
-            }
+            if (p[0] == p[5] & p[10] == p[7] & p[5] == p[10] & p[0]!=5)
+                ZigZag(p[0]);
+            if (p[1] == p[4] & p[6] == p[11] & p[4] == p[6] & p[1] != 5)
+                ZigZag(p[1]);
+            if (p[2] == p[5] & p[8] == p[7] & p[5] == p[8] & p[2] != 5)
+                ZigZag(p[2]);
+            if (p[3] == p[6] & p[9] == p[4] & p[6] == p[9] & p[3]!=5)
+                ZigZag(p[3]);
 
-            player.getCredit();
+            credits = credits + total;
 
-            WinLabel.Text = "Win: " + player.Total.ToString();
-            creditLabel.Text = "Credits: " + player.Credits.ToString();
-            BetLabel.Text = "Bet: " + player.Bet.ToString();
+
+            WinLabel.Text = "Win: " + total.ToString();
+            creditLabel.Text = "Credits: " + credits.ToString();
+            BetLabel.Text = "Bet: " + bet.ToString();
         }
 
         private void Rolling()
         {
             for (int i = 0; i < 12; i++)
             {
-                p[i] = Randomizer.getRand(1, 9);
+                p[i] = Randomizer.getRand(1, 6);
 
                 if (i >= 4)
                 {
-                    if (p[i] == p[i - 4] & p[i] == 8)
+                    if (p[i] == p[i - 4] & p[i] == 5)
                     {
-                        p[i] = Randomizer.getRand(1, 8);
-                    }
-                    if(i >= 8)
-                    {
-                        if (p[i] == p[i - 8] & p[i] == 8)
-                        {
-                            p[i] = Randomizer.getRand(1, 8);
-                        }
+                        p[i] = Randomizer.getRand(1, 5);
                     }
                 }
             }
 
+
+            //p1 = Randomizer.getRand(1, 5);
+            //p2 = Randomizer.getRand(1, 5);
+            //p3 = Randomizer.getRand(1, 5);
+            //p4 = Randomizer.getRand(1, 5);
+
+            //p5 = Randomizer.getRand(1, 5);
+            //p6 = Randomizer.getRand(1, 5);
+            //p7 = Randomizer.getRand(1, 5);
+            //p8 = Randomizer.getRand(1, 5);
+
+            //p9 = Randomizer.getRand(1, 5);
+            //p10 = Randomizer.getRand(1, 5);
+            //p11 = Randomizer.getRand(1, 5);
+            //p12 = Randomizer.getRand(1, 5);
+
             if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
-            pictureBox1.Image = Image.FromFile("Pictures/" + p[0].ToString() + ".png");
+            pictureBox1.Image = Image.FromFile(p[0].ToString() + ".png");
             if (pictureBox2.Image != null) pictureBox2.Image.Dispose();
-            pictureBox2.Image = Image.FromFile("Pictures/" + p[1].ToString() + ".png");
+            pictureBox2.Image = Image.FromFile(p[1].ToString() + ".png");
             if (pictureBox3.Image != null) pictureBox3.Image.Dispose();
-            pictureBox3.Image = Image.FromFile("Pictures/" + p[2].ToString() + ".png");
+            pictureBox3.Image = Image.FromFile(p[2].ToString() + ".png");
             if (pictureBox4.Image != null) pictureBox4.Image.Dispose();
-            pictureBox4.Image = Image.FromFile("Pictures/" + p[3].ToString() + ".png");
+            pictureBox4.Image = Image.FromFile(p[3].ToString() + ".png");
 
             if (pictureBox5.Image != null) pictureBox5.Image.Dispose();
-            pictureBox5.Image = Image.FromFile("Pictures/" + p[4].ToString() + ".png");
+            pictureBox5.Image = Image.FromFile(p[4].ToString() + ".png");
             if (pictureBox6.Image != null) pictureBox6.Image.Dispose();
-            pictureBox6.Image = Image.FromFile("Pictures/" + p[5].ToString() + ".png");
+            pictureBox6.Image = Image.FromFile(p[5].ToString() + ".png");
             if (pictureBox7.Image != null) pictureBox7.Image.Dispose();
-            pictureBox7.Image = Image.FromFile("Pictures/" + p[6].ToString() + ".png");
+            pictureBox7.Image = Image.FromFile(p[6].ToString() + ".png");
             if (pictureBox8.Image != null) pictureBox8.Image.Dispose();
-            pictureBox8.Image = Image.FromFile("Pictures/" + p[7].ToString() + ".png");
+            pictureBox8.Image = Image.FromFile(p[7].ToString() + ".png");
 
             if (pictureBox9.Image != null) pictureBox9.Image.Dispose();
-            pictureBox9.Image = Image.FromFile("Pictures/" + p[8].ToString() + ".png");
+            pictureBox9.Image = Image.FromFile(p[8].ToString() + ".png");
             if (pictureBox10.Image != null) pictureBox10.Image.Dispose();
-            pictureBox10.Image = Image.FromFile("Pictures/" + p[9].ToString() + ".png");
+            pictureBox10.Image = Image.FromFile(p[9].ToString() + ".png");
             if (pictureBox11.Image != null) pictureBox11.Image.Dispose();
-            pictureBox11.Image = Image.FromFile("Pictures/" + p[10].ToString() + ".png");
+            pictureBox11.Image = Image.FromFile(p[10].ToString() + ".png");
             if (pictureBox12.Image != null) pictureBox12.Image.Dispose();
-            pictureBox12.Image = Image.FromFile("Pictures/" + p[11].ToString() + ".png");
+            pictureBox12.Image = Image.FromFile(p[11].ToString() + ".png");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Rolling();
-            if (musicImage.Tag.Equals("unmute.png") && musicPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+            if (musicImage.Tag.Equals("unmute.png") && player.playState == WMPLib.WMPPlayState.wmppsStopped)
             {
-                musicPlayer.URL = "OogwayAscends.mp3";
-                musicPlayer.controls.play();
+                player.URL = "OogwayAscends.mp3";
+                player.controls.play();
             }
         }
 
         private void BetAmountNumeric_ValueChanged(object sender, EventArgs e)
         {
-            if (BetAmountNumeric.Value > Convert.ToInt32(player.Credits))
+            if (BetAmountNumeric.Value > Convert.ToInt32(credits))
             {
-                BetAmountNumeric.Value = Convert.ToInt32(player.Credits);
+                BetAmountNumeric.Value = Convert.ToInt32(credits);
             }
             if (BetAmountNumeric.Value >= 5)
             {
-                player.Bet = Convert.ToInt64(BetAmountNumeric.Value);
+                bet = Convert.ToInt64(BetAmountNumeric.Value);
             }
 
         }
@@ -217,24 +317,15 @@ namespace Proekt_test2_VP
             {
                 musicImage.Image = Image.FromFile("unmute.png");
                 musicImage.Tag = "unmute.png";
-                musicPlayer.URL = "OogwayAscends.mp3";
-                musicPlayer.controls.play();
+                player.URL = "OogwayAscends.mp3";
+                player.controls.play();
             }
             else
             {
                 musicImage.Image = Image.FromFile("mute.png");
                 musicImage.Tag = "mute.png";
-                musicPlayer.controls.stop();
+                player.controls.stop();
             }
-        }
-
-        private void FeatureButton_Click(object sender, EventArgs e)
-        {
-            player.SpinsLeft = 10;
-            BonusSpins newBonusSpins = new BonusSpins(player);
-            this.Hide();
-            newBonusSpins.ShowDialog();
-            this.Close();
         }
     }
 }
